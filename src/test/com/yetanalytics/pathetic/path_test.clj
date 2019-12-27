1aaffbc03f1add47c7fe63402757d029dfc15b69
Milt Reder <milt@yetanalytics.com>
2019-12-27 15:49:46 -0500

    more tests for path

--- src/test/com/yetanalytics/pathetic/path_test.clj
+++ src/test/com/yetanalytics/pathetic/path_test.clj
@@ -15,14 +15,21 @@
     [r (io/reader (io/resource "pathetic/data/long.json"))]
     (json/read r)))
 
-;; Explode a statement using a helper from our zipperoo
+
 (deftest path->spec-test
-  (is (every?
-       (fn [[path v]]
-         (let [spec (path->spec ::xs/statement path long-statement)]
-           (and spec
-                (s/valid? spec v))))
-       (pzip/json->path-map long-statement)) ))
+  (testing "works for lots of paths"
+    ;; Explode a statement using a helper from our zipperoo to get a bunch of
+    ;; paths and leaf values
+    (is (every?
+         (fn [[path v]]
+           (let [spec (path->spec ::xs/statement path long-statement)]
+             (and spec
+                  (s/valid? spec v))))
+         (pzip/json->path-map long-statement))))
+
+  (testing "works for arbitrary and relative paths"
+    (is (= ::xs/language-map-text
+           (path->spec ::xs/activity ["definition" "name" "en-US"])))))
 
 
 (comment
