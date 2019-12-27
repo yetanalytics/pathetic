ea58fcf7bfec2082b144c134c3bab7fe883079f1
Milt Reder <milt@yetanalytics.com>
2019-12-27 15:43:21 -0500

    added path-to-spec function

---
+++ src/test/com/yetanalytics/pathetic/path_test.clj
@@ -0,0 +1,39 @@
+(ns com.yetanalytics.pathetic.path-test
+  (:require [clojure.test :refer :all]
+            [com.yetanalytics.pathetic.path :refer :all]
+            [xapi-schema.spec :as xs]
+            [clojure.spec.alpha :as s]
+            [com.yetanalytics.pathetic.zip :as pzip]
+            [clojure.java.io :as io]
+            [clojure.data.json :as json]))
+
+(deftest spec-map-test
+  (is (s/valid? :com.yetanalytics.pathetic.path/spec-map spec-map)))
+
+(def long-statement
+  (with-open
+    [r (io/reader (io/resource "pathetic/data/long.json"))]
+    (json/read r)))
+
+;; Explode a statement using a helper from our zipperoo
+(deftest path->spec-test
+  (is (every?
+       (fn [[path v]]
+         (let [spec (path->spec ::xs/statement path long-statement)]
+           (and spec
+                (s/valid? spec v))))
+       (pzip/json->path-map long-statement)) ))
+
+
+(comment
+  (path->spec ::xs/statement ["actor" "member"] long-statement)
+  (path->spec ::xs/statement
+              ["object" "definition" "extensions" "http://example.com/profiles/meetings/activitydefinitionextensions/room" "id"] long-statement)
+
+
+  (path->spec ::xs/statement
+              ["result" "extensions" "http://example.com/profiles/meetings/resultextensions/minuteslocation"] long-statement)
+  long-statement
+
+
+  )
